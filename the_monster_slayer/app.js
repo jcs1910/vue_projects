4a3b9d087be180f4ab4f3093861550ac77591c64
dokdo <jcs191072@gmail.com>
2020-09-11 15:51:11 +0900

    feat: Implementing a 'Attack' Method and add its logic

--- the_monster_slayer/app.js
+++ the_monster_slayer/app.js
@@ -10,6 +10,27 @@ new Vue({
       this.gameIsRunning = true;
       this.playerHealth = 100;
       this.monsterHealth = 100;
+    },
+    attack() {
+      let max = 10;
+      let min = 3;
+      let damage = Math.max(Math.floor(Math.random() * max + 1), min);
+      
+      this.monsterHealth -= damage
+
+      if (this.monsterHealth <= 0) {
+        alert('You won!!!')
+      }
+
+      max = 12;
+      min = 5;
+      damage = Math.max(Math.floor(Math.random() * max + 1), min);
+
+      this.playerHealth -= damage
+
+      if (this.playerHealth <= 0) {
+        alert('You Lost!!!')
+      }
     }
   }
 })
