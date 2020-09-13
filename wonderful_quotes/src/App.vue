d193375ca1dc26623f9f8be2a27846e4201bbfab
dokdo <jcs191072@gmail.com>
2020-09-13 16:37:28 +0900

    feat: Passing Data with Props and Slots

--- wonderful_quotes/src/App.vue
+++ wonderful_quotes/src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div class="container">
-
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
+import QuoteGrid from './components/QuoteGrid.vue';
 export default {
     data() {
         return {
-            quotes: [],
+            quotes: ['hello world'],
             maxQuotes: 10
         }
+    },
+    components: {
+        appQuoteGrid: QuoteGrid
     }
 }
 </script>
